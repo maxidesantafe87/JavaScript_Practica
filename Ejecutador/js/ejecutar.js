590e24db0103df2859cc725f7647f6a1712bd1db
maxidesantafe87 <maxidesantafe87@gmail.com>
2023-02-17 13:36:28 -0300

    ACT

---
+++ Ejecutador/js/ejecutar.js
@@ -0,0 +1,10 @@
+function ejecutarCodigo() {
+    // Obtener el contenido del cuadro de texto
+    var codigo = document.getElementById("codigo-js").value;
+  
+    // Ejecutar el código usando eval()
+ 
+      var resultado = eval(codigo);
+      
+   
+  }
